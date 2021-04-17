4b784820c612b2773cb5dc5107b1804e309d67fe
maja-wright <maja.filipasic@hotmail.com>
2021-04-17 10:26:07 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const button = document.querySelector("button");
+
+button.addEventListener('click', rollTheDice);
+
+function rollTheDice(){
+
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomImageSource = "/images/dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    let randomImageSource2 = "/images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+    if (randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+}
+
+
+    
