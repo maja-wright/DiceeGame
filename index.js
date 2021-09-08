cae74b93772b808b1b90c0113c704ff9f84d0bb1
maja-wright <maja.filipasic@hotmail.com>
2021-09-08 12:38:00 -0700

    images file

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ button.addEventListener('click', rollTheDice);
 function rollTheDice(){
 
     let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    let randomImageSource = "/images/dice" + randomNumber1 + ".png";
+    let randomImageSource = "images/dice" + randomNumber1 + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
     let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-    let randomImageSource2 = "/images/dice" + randomNumber2 + ".png";
+    let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
     if (randomNumber1 > randomNumber2){
